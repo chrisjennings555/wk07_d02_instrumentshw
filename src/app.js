38b8ae081c4b31dca68ce3986fa51421cd877218
Chris Jennings <chrisjennings555@gmail.com>
2018-11-27 20:40:20 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const InstrumentFamilies = require('./models/instrument_families.js');
+const InstrumentSelectView = require('./views/instrument_select_view');
 const InstrumentInfoView = require('./views/instrument_info_view.js');
 
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
-  const selectElement = document.querySelector('#instrument-families');
-  const instrumentDropdown = new InstrumentInfoView(selectInstrument);
+  const selectElement = document.querySelector('select#instrument-families');
+  const instrumentDropdown = new InstrumentSelectView(selectElement);
   instrumentDropdown.bindEvents();
+
+  const instrumentInfoDiv = document.querySelector('div.instrument-info');
+  const instrumentInfoDisplay = new InstrumentInfoView(instrumentInfoDiv);
+  instrumentInfoDisplay.bindEvents();
+
+  const instrumentDataSource = new InstrumentFamilies();
+  instrumentDataSource.bindEvents();
 });
--- src/views/Instrument_info_view.js
+++ src/views/Instrument_info_view.js
@@ -1,7 +1,7 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const InstrumentInfoView = function (){
-
+const InstrumentInfoView = function (container){
+  this.container = container;
 };
 
 InstrumentInfoView.prototype.bindEvents = function() {
@@ -10,3 +10,17 @@ InstrumentInfoView.prototype.bindEvents = function() {
     this.render(instrument);
   });
 };
+
+InstrumentInfoView.prototype.render = function(instrument){
+  const headerFamily = document.createElement('h2');
+  headerFamily.textContent = `${instrument.name}`;
+
+  const familyInfoParagraph = document.createElement('h3');
+  familyInfoParagraph.textContent = `${instrument.description}`;
+
+  this.container.innerHTML = '';
+  this.container.appendChild(headerFamily);
+  this.container.appendChild(familyInfoParagraph);
+};
+
+module.exports = InstrumentInfoView;
